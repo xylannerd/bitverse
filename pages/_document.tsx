06b8a91472194fe5e55a50cd3d501a5e153ea662
xylan <xylannerd@gmail.com>
2022-02-20 17:43:20 +0530

    stylesheets using next/head inside _document instead

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,17 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+         <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;500&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Oswald:wght@500&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
         <body>
           <Main />
           <NextScript />
--- pages/components/navComponent/navbar.tsx
+++ pages/components/navComponent/navbar.tsx
@@ -129,17 +129,7 @@ const Navbar: React.FC = () => {
 
   return (
     <>
-      <Head>
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;500&display=swap"
-          rel="stylesheet"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Oswald:wght@500&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
+     
       <div
         id="navBar"
         className="flex items-center w-full h-16 bg-black shadow-md"
